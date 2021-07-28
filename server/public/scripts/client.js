27237ab092dfc27c2dee036d37355ea5f54ce28c
Michael Werneke <mike.werneke@gmail.com>
2021-07-28 16:13:54 -0500

    renderDOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,28 @@ $(document).ready (onReady);
 
 function onReady(){
     console.log( 'Jquery');
+    getQuotes();
 }
+
+function getQuotes(){
+    
+    $.ajax({
+        url:'/quotes',
+        method: 'GET',
+    })
+        .then(function(response){
+            console.log('GET /quotes response',response);
+        
+       //render
+       let quotesList =$(`#quotes`); 
+       for(let quote of response){
+           quotesList.append(`
+           <li>
+            Quote: ${quote.text}
+                <em> By: ${quote.author} </em>
+           </li>
+           `)
+       };
+    }
+)};          
+
