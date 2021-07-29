0f20a22bd88016edab2d57e5252af361a99e89d9
Michael Werneke <mike.werneke@gmail.com>
2021-07-29 10:35:20 -0500

    done

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,11 +1,33 @@
 $(document).ready (onReady);
 
+
+
+
 function onReady(){
     console.log( 'Jquery');
     getQuotes();
+    console.log($(`#submitBtn`));
+    $(`#submitBtn`).on(`click`, addQuote)
+};
+
+function addQuote(){
+    console.log('inside add Quote');
+
+
+//Grab Data from DOM Inputs
+let newQuote ={
+    text:$(`#quoteInput`).val(),
+    author:$(`#authorInput`).val()
+};
+    console.log('New Quote is a Quote', newQuote);
+    
+//POST our quote
 }
 
+
+
 function getQuotes(){
+    
     
     $.ajax({
         url:'/quotes',
